6b057cf3b4b1ab22e898533194163025c4b85ab4
RozkeAdine <rozke-adine.bouyahia-std@donkey.school>
2022-07-12 12:29:54 +0200

    exercice 3

--- Challenge03JS.js
+++ Challenge03JS.js
@@ -18,13 +18,20 @@ console.log("exercice2")
 if (imcBernard < imcMarcel) {
     console.log("Marcel à un IMC(" + imcMarcel.toFixed(2) + ") plus élevé que Bernard (" + imcBernard.toFixed(2) + ")") 
 } else {
-    console.log("Bernard à un IMC(" + imcBernard.toFixed(2) + ") plus élevé que Marcel(" + imcMarcel.toFixed(2) + ")")
+    console.log("Bernard à un IMC(" + imcBernard.toFixed(1) + ") plus élevé que Marcel(" + imcMarcel.toFixed(0) + ")")
 };
 
 console.log("exercice3")
-console.log("exercice2")
-if (imcBernard < imcMarcel) {
-    console.log("Marcel à un IMC(" + imcMarcel.toFixed(2) + ") plus élevé que Bernard (" + imcBernard.toFixed(2) + ")") 
-} else {
-    console.log("Bernard à un IMC(" + imcBernard.toFixed(2) + ") plus élevé que Marcel(" + imcMarcel.toFixed(2) + ")")
-};
+
+if (imcBernard<20){
+        console.log("Bernard : insufisance pondérale")
+    }
+if(imcMarcel<20){
+        console.log("Marcel : insufisance pondérale")
+    }
+if ((imcBernard>28) && (imcBernard<33)){
+        console.log("Bernard : Surpoid")
+    }
+if ((imcMarcel>28) && (imcMarcel<33)){
+        console.log("Marcel : Surpoid")
+    };
